106462f439e93cdbbe7561ee9192176cb34f96a6
Nahid51 <mdnahidyn@gmail.com>
2023-11-01 00:24:26 +0600

    added clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from "react";
 import "../styles/client.scss";
+import MotionRight from "./MotionRight";
 
 const Clients = () => {
+
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('/clients.json')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No profile data</p>
+
     return (
-        <div id="clients" className="clientBg">Clients</div>
+        <div id="clients" className="clientBg">
+            <div>
+                <div className="clientHeader">
+                    <h4>Clients</h4>
+                    <h1>Our Client</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, atque.</p>
+                </div>
+                <div className="clientDetails">
+                    {
+                        data.map((item, i) => (
+                            <MotionRight key={i} >
+                                <div className="clientImage">
+                                    <img src={item.image} alt="clientImage" />
+                                </div>
+                            </MotionRight>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
     )
 };
 
