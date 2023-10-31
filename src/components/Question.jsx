14d323d96297535988827e402b97f0e8e907b1da
Nahid51 <mdnahidyn@gmail.com>
2023-10-31 22:29:05 +0600

    added question in mind section

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,30 @@
-
+import "../styles/question.scss";
+import { Link } from "react-router-dom";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+import MotionLeft from "./MotionLeft";
+import MotionRight from "./MotionRight";
 
 const Question = () => {
     return (
-        <div>Question</div>
+        <div>
+            <section className="questionInMindBg">
+                <div className="questionSection">
+                    <div className="questionAlignment">
+                        <MotionLeft>
+                            <div style={{ color: "white" }}>
+                                <h2 style={{ fontSize: "2rem" }}>Have a project in mind?</h2>
+                                <p style={{ fontSize: "1rem", marginTop: ".5rem" }} className="text-white font-medium text-lg mt-3">{`Let's get to work`}</p>
+                            </div>
+                        </MotionLeft>
+                        <MotionRight>
+                            <Link to="#" style={{ textDecoration: "none" }}>
+                                <button className="questionButton"><span>Contact Us</span> <MdOutlineArrowForwardIos /></button>
+                            </Link>
+                        </MotionRight>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,9 +18,9 @@ const Home = () => {
             <About />
             <Services />
             <Products />
+            <Question />
             <Projects />
             <Testimonials />
-            <Question />
             <MeetTheTeam />
             <Clients />
             <Footer />
